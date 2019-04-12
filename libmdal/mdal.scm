54085dc542bcb209c5f7e35aef237bde1967ffb2
utz <utz82@users.noreply.github.com>
2019-04-12 13:48:01 +0200

    move top-level compiler fns to main mdal module (for now)

--- libmdal/mdal.scm
+++ libmdal/mdal.scm
@@ -4,6 +4,33 @@
 
 (module mdal *
 
-  (import scheme chicken)
+  (import scheme chicken srfi-4)
   (use mdal-core)
-  (reexport mdal-core))
+  (reexport mdal-core)
+
+  ;;----------------------------------------------------------------------------
+  ;;; ### output compilation
+  ;;----------------------------------------------------------------------------
+
+  ;;; compile an md:module to an onode tree
+  ;;; TODO and a list of symbols for mod->asm?
+  (define (md:mod-compile mod origin)
+    ((md:config-compiler (md:mod-cfg mod)) mod origin))
+
+  ;;; compile an md:module into a bytevec
+  (define (md:mod->bin mod origin)
+    (md:otree->bin (md:mod-compile mod origin)))
+
+  ;;; compile an md:module into an assembly source
+  (define (md:mod->asm mod origin)
+    (let ((otree (md:mod-compile mod origin)))
+      '()))
+
+  ;;; compile the given md:module to a binary file
+  (define (md:mod-export-bin filename mod origin)
+    (call-with-output-file filename
+      (lambda (port)
+	(write-u8vector (list->u8vector (md:mod->bin mod origin))
+			port))))
+
+  ) ;; end module mdal
