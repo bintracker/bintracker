5246c21b4a5f4d07be9c91db851275e2766fd163
utz <utz82@users.noreply.github.com>
2019-04-14 23:29:23 +0200

    start implementing bintracker gui

---
+++ config.scm
@@ -0,0 +1,10 @@
+;; -*- geiser-scheme-implementation: 'chicken -*-
+
+;; -----------------------------------------------------------------------------
+;; BINTRACKER CONFIGURATION
+;; -----------------------------------------------------------------------------
+
+(setconf! 'keymap "DE")
+
+(setconf! 'color-row-hl "light-grey")
+(setconf! 'color-row-hl "dark-grey")
