a0cd22783765394cd26899885d165848a9a9dc08
utz <utz82@users.noreply.github.com>
2020-06-15 14:56:14 +0200

    add support for Sharp MZ-700

--- config/systems.scm
+++ config/systems.scm
@@ -4,4 +4,5 @@
  ("channelf" emulator: "mame64"
   startup-args: ("channelf" "-cart" "mame-bridge/channelf_dummy.bin")
   pc-name: "PC0" loader-type: cart default-run-address: #x802)
+ ("mz700" emulator: "mame64" startup-args: ("mz700"))
  ("spectrum48" emulator: "mame64" startup-args: ("spectrum")))
---
+++ libmdal/mdal-targets/mz700.scm
@@ -0,0 +1,5 @@
+(id: "mz700"
+     cpu: z80
+     clock-speed: 3546894
+     default-start-address: #x1200
+     exports: ())
