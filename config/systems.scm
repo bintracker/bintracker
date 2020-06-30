e30f49021f52c2915501014407d12bfce23ccb10
utz <utz82@users.noreply.github.com>
2020-06-30 15:45:33 +0200

    add support for Exidy Sorcerer

--- config/systems.scm
+++ config/systems.scm
@@ -5,4 +5,5 @@
   startup-args: ("channelf" "-cart" "mame-bridge/channelf_dummy.bin")
   pc-name: "PC0" loader-type: cart default-run-address: #x802)
  ("mz700" emulator: "mame64" startup-args: ("mz700"))
- ("spectrum48" emulator: "mame64" startup-args: ("spectrum")))
+ ("spectrum48" emulator: "mame64" startup-args: ("spectrum"))
+ ("sorcerer" emulator: "mame64" startup-args: ("sorcerer")))
---
+++ libmdal/mdal-targets/sorcerer.scm
@@ -0,0 +1,5 @@
+(id: "sorcerer"
+     cpu: z80
+     clock-speed: 2106000
+     default-start-address: #x100
+     exports: ())
