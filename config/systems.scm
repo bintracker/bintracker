efb90b5276bbc911df90ef3098de92092b945462
utz <utz82@users.noreply.github.com>
2020-07-15 17:36:39 +0200

    add support for Atari VCS/2600 (NTSC)

--- config/systems.scm
+++ config/systems.scm
@@ -1,6 +1,7 @@
 (;; TODO better dummy cart that disables sound on startup
  ("atari2600" emulator: "mame64"
-  startup-args: ("a2600" "-cart" "mame-bridge/a2600_dummy.bin"))
+  startup-args: ("a2600" "-cart" "mame-bridge/a2600_dummy.bin")
+  loader-type: cart default-run-address: #xf000)
  ("channelf" emulator: "mame64"
   startup-args: ("channelf" "-cart" "mame-bridge/channelf_dummy.bin")
   pc-name: "PC0" loader-type: cart default-run-address: #x802)
---
+++ libmdal/mdal-targets/atari2600.scm
@@ -0,0 +1,5 @@
+(id: "atari2600"
+     cpu: m6502
+     clock-speed: 1190000
+     default-start-address: #xf000
+     exports: ())
