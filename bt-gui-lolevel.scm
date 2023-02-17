1a3abdd6b3723fc4541997c71c0cbbdc3ddf1605
utz <utz82@users.noreply.github.com>
2023-02-17 12:56:07 +0100

    lock on tk/destroy
    Another attempt at dealing with #13.

--- bt-gui-lolevel.scm
+++ bt-gui-lolevel.scm
@@ -779,7 +779,7 @@
   ;;; after calling this method.
   (define-method (ui-destroy primary: (elem <ui-element>))
     (for-each (o ui-destroy cdr) (ui-children elem))
-    (tk/destroy (ui-box elem)))
+    (tk-with-lock (lambda () (tk/destroy (ui-box elem)))))
 
   ;;; Returns ELEMs child UI element with the identifier CHILD-ELEMENT. The
   ;;; requested element may be a direct descendant of ELEM, or an indirect
