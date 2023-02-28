614617789e71cbb31933cfc48b6234c3e7a5467a
utz <utz82@users.noreply.github.com>
2023-02-28 23:41:17 +0100

    disable useless export menu entries

--- plugins/extra-formats/extra-formats.scm
+++ plugins/extra-formats/extra-formats.scm
@@ -11,21 +11,41 @@
 	 'add
 	 'extra-formats::register-menu-entries
 	 (lambda ()
-	   (add-menu-item!
-	    (alist-ref 'export
-		       (menu-items (alist-ref 'file
-					      (menu-items (state 'menu)))))
-	    `(command zx-tap
-		      ".tap (ZX Spectrum)"
-		      1
-		      #f
-		      ,extra-formats::zx-tap-dialog))
-	   (add-menu-item!
-	    (alist-ref 'export
-		       (menu-items (alist-ref 'file
-					      (menu-items (state 'menu)))))
-	    `(command dragon-cas
-		      ".cas (Dragon/Tandy)"
-		      1
-		      #f
-		      ,extra-formats::dragon-cas-dialog))))))
+	   (let ((export-menu
+		  (alist-ref 'export
+			     (menu-items
+			      (alist-ref 'file
+					 (menu-items (state 'menu)))))))
+	     (for-each (lambda (id label proc)
+			 (add-menu-item!
+			  export-menu
+			  `(command ,id ,label 1 #f ,proc))
+			 ((menu-widget export-menu) 'entryconfigure
+			  label state: 'disabled))
+		       '(zx-tap dragon-cas)
+		       '(".tap (ZX Spectrum)" ".cas (Dragon/Tandy)")
+		       `(,extra-formats::zx-tap-dialog
+			 ,extra-formats::dragon-cas-dialog))
+	     (on-close-file-hooks
+	      'add
+	      'disable-extra-formats-menu-entries
+	      (lambda ()
+		(for-each (lambda (label)
+			    ((menu-widget export-menu) 'entryconfigure
+			     label state: 'disabled))
+			  '(".tap (ZX Spectrum)" ".cas (Dragon/Tandy)"))))
+	     (after-load-file-hooks
+	      'add
+	      'enable-extra-formats-menu-entries
+	      (lambda args
+		(let ((platform (string->symbol
+  				 (target-platform-id
+				  (mdef-target (current 'mdef))))))
+		  (for-each (lambda (platform-list label)
+			      (when (memv platform platform-list)
+				((menu-widget export-menu) 'entryconfigure
+				 label state: 'normal)))
+			    '((spectrum16 spectrum48 spectrum128)
+			      (dragon32 dragon64 coco3 mc10))
+			    '(".tap (ZX Spectrum)"
+			      ".cas (Dragon/Tandy)"))))))))))
