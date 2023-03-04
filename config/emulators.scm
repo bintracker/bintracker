856708c08b89040af1efecb8ee9dd490b3db9e5b
utz <utz82@users.noreply.github.com>
2023-03-04 16:30:40 +0100

    pass -uimodekey to MAME
    Now users can access the in-emulation menu in MAME by pressing F12, Tab.

--- config/emulators.scm
+++ config/emulators.scm
@@ -3,6 +3,7 @@
        default-args: ("-w" "-skip_gameinfo" "-autoboot_script"
 		      "mame-bridge/mame-startup.lua"
 		      "-rompath" "roms"
+		      "-uimodekey" "F12"
 		      "-nomouse"
 		      "-mouse_device" "none"
 		      "-autoboot_delay" "0")))
